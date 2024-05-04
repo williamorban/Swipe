b4e3b2f2271dbfb38e2633726cbbf3158daadade
williamorban <90011394+williamorban@users.noreply.github.com>
2024-05-03 23:11:22 -0500

    updated class, WIP.

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from customtkinter import *
+import csv
+root = CTk()
+root.geometry("500x500")
+
+class ctk(CTk):
+   None
+
+CTk = ctk
+
+frame_home = ctk.frame(root)
+
+root.mainloop()
